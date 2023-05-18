526a6d5585e4a62e4275cb03767bb1ed7dc20233
Abby <70772021+stuckia@users.noreply.github.com>
2023-05-18 12:12:40 -0400

    Transfer from private to public

---
+++ Visualizer/src/StudentMain.java
@@ -0,0 +1,12 @@
+public class StudentMain
+{
+	// START *CODING* HERE.  Your program MUST start in THIS main method.
+	// You are welcome to add your own helper classes and use whatever
+	// classes we provide in you project.  But your program must start with
+	// this main method.
+
+	public static void main(String[] args)
+	{
+		// START CODING HERE
+	}
+}
---
+++ Visualizer/src/readme.txt
@@ -0,0 +1,7 @@
+Your documentation for your visualizer goes here.
+
+You must include:
+1) Instructions to us on how to run your program
+2) Instructions to us on how to customize the data that gets visualized
+3) Any known issues, and how to work around them, so we can see as much
+	of your project as possible.
